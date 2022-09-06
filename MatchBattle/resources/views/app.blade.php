a70760972975d1c62593f549cefdee714653bb42
David Caro <david.caro1@misena.edu.co>
2022-09-06 15:07:44 -0500

    docs: index_view

--- MatchBattle/resources/views/app.blade.php
+++ MatchBattle/resources/views/app.blade.php
@@ -5,9 +5,85 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{url('css/index.css')}}">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <title>Jugar</title>
 </head>
 <body>
-hola mundo
+
+<div class="global_container">
+    <div class="navbar">
+        <div class="logo">
+            <img src="{{url('img/logo.svg')}}" alt="">
+        </div>
+        <div class="profile">
+            <div><p>Elige tu avatar</p></div>
+            <div><img src="{{url('img/perfil.png')}}" alt=""></div>
+
+        </div>
+    </div>
+    <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+        <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+        </ol>
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="carrousel_content_1">
+
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="carrousel_content_1">
+
+                </div>
+            </div>
+
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    <br>
+    <div class="options">
+        <div class="input-group">
+            <input type="text" name="text" autocomplete="off" class="input" required>
+            <label class="user-label">First Name</label>
+        </div>
+        <br>
+        <button class="button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span> Unirme a sala
+        </button>
+        <br>
+        <button class="button">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span> Crear sala
+        </button>
+
+    </div>
+
+</div>
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
+        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
+        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
+        crossorigin="anonymous"></script>
+
 </body>
 </html>
